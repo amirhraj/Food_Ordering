d96480daa35b673cc69afec7629ee7900a22841c
Radgab <radgab712@gmail.com>
2023-12-27 19:50:21 +0300

    -add

---
+++ food_ordering/src/components/App.js
@@ -0,0 +1,146 @@
+
+ import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import logo from '../media/icon/Food_Offer.svg';
+import  Burger from '../media/icon/hambergermenu.svg';
+import Geo from '../media/icon/location.svg';
+import Profile from '../media/menu/Rectangle 20.png';
+import Arrow from '../media/icon/arrowdown2.svg';
+import Search from '../media/icon/searchnormal1.svg';
+import Settings from '../media/icon/setting5.svg';
+import burger from '../media/menu/Rectangle 22.png';
+import pizza from '../media/menu/Rectangle 23.png';
+import pasta from '../media/menu/Rectangle 24.png';
+import sandwich from '../media/menu/Rectangle 25.png';
+import fri from '../media/menu/Rectangle 26.png';
+import kebab from '../media/menu/Rectangle 27.png';
+import Vegan from '../media/micro_menu/vegan.png';
+import Cake from '../media/micro_menu/Cake.png';
+import Coffe from '../media/micro_menu/Coffe.png';
+import Dessert from '../media/micro_menu/Dessert.png';
+import Fast from '../media/micro_menu/fast_food.png';
+import Kebab from '../media/micro_menu/Kebab.png';
+import Salad from '../media/micro_menu/Salad.png';
+import Sea_food from '../media/micro_menu/sea_food.png';
+import Heart from '../media/icon/heart.png';
+import home from '../media/icon/home.svg';
+import like from '../media/icon/like.png';
+import notification from '../media/icon/notification.png';
+import profilecircle from '../media/icon/profilecircle.png';
+import buscket from '../media/icon/shoppingcart.png';
+
+
+function App() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="logo" src={logo} alt="Иконка" />
+        <div className="wraper_group_header">
+            <div className='geolocation'> 
+            <img className="burger" src={Burger} alt="Меню" />
+              <img className='geo' src={Geo} alt="" />
+                <p>Paris, France</p>
+                <img className='arrow' src={Arrow} alt="" />
+              <img className='profile' src={Profile} alt="" />
+            </div>
+        </div>
+        <div className='text'> 
+            <p className='header_button_text'>Delicious Food? </p>
+            <p className='header_button_text2'>Go Ahead...</p>
+        </div>
+        <div className='search'>
+            <input className='header_input' placeholder="Search for your favourite food" type="text" />
+            <img className='header_search' src={Search} alt="" />
+            <img className='header_settings' src={Settings} alt="" />
+        </div>
+        
+      </header>
+      <body  className ="content">
+          <div className='content_promo_offer'>
+            <p className='promo_text'>30% Off on your first purchase</p>
+          </div>
+          <div className='content_menu'>
+            <img src={burger} alt="burger" />
+            <img src={pizza} alt="pizza" />
+            <img src={pasta} alt="pasta" />
+            <img src={sandwich} alt="andwich" />
+            <img src={fri} alt="fri" />
+            <img src={kebab} alt="kebab" />
+          </div>
+          <button className='see_more'>See More... </button>
+          <div className='content_mini_menu'>
+              <img src={Vegan} alt="burger" />
+              <img src={Sea_food} alt="pizza" />
+              <img src={Fast} alt="pasta" />
+              <img src={Kebab} alt="pasta" />
+              <img src={Salad} alt="andwich" />
+              <img src={Dessert} alt="fri" />
+              <img src={Cake} alt="kebab" />
+              <img src={Coffe} alt="kebab" />
+          </div>
+      </body>
+      <footer className = "footer_content">
+       <div className="fab-container">
+        <ul>
+        <li
+          className={`fab-item ${activeIndex === 0 ? 'active' : ''}`}
+          onClick={() => handleItemClick(0)}
+        >
+          <a href="#">
+          <img src={home} alt="" />
+          </a>
+         
+        </li>
+        <li
+          className={`fab-item ${activeIndex === 1 ? 'active' : ''}`}
+          onClick={() => handleItemClick(1)}
+        >
+          <a href="#">
+          <img src={like} alt="" />
+          </a>
+         
+        </li>
+        <li
+          className={`fab-item ${activeIndex === 2 ? 'active' : ''}`}
+          onClick={() => handleItemClick(2)}
+        >
+          <a href="#">
+          <img src={buscket} alt="" />
+          </a>
+         
+        </li>
+        <li
+          className={`fab-item ${activeIndex === 3 ? 'active' : ''}`}
+          onClick={() => handleItemClick(3)}
+        >
+          <a href="#">
+          <img src={notification} alt="" />
+          </a>
+         
+        </li>
+        <li
+          className={`fab-item ${activeIndex === 4 ? 'active' : ''}`}
+          onClick={() => handleItemClick(4)}
+        >
+          <a href="#">
+          <img src={profilecircle} alt="" />
+          </a>
+        </li>
+          <div className='indicator'></div>
+        </ul>
+        </div>
+      </footer>
+    </div>
+  );
+
+
+}
+
+export default App;
---
+++ food_ordering/src/components/General.js
@@ -0,0 +1,115 @@
+
+import React, { useState, useEffect } from 'react';
+import  '../styled/General_style.css'
+import star from '../media/menu/Stars.png';
+import Rest_italia from '../media/menu/Italia.png';
+import Rest_Kebab from '../media/menu/Traditional_kebab.png';
+import Rest_fish from '../media/menu/Star_fish.png';
+import Rest_Burger from '../media/menu/Buster_Burger.png';
+import mini_star from '../media/menu/mini_stars.svg';
+import home from '../media/icon/home.svg';
+import like from '../media/icon/like.png';
+import notification from '../media/icon/notification.png';
+import profilecircle from '../media/icon/profilecircle.png';
+import buscket from '../media/icon/shoppingcart.png';
+
+function getRandomColor() {
+    const getRandomValue = () => Math.floor(Math.random() * 256);
+    return `rgb(${getRandomValue()}, ${getRandomValue()}, ${getRandomValue()})`;
+  }
+
+function General (){
+    const [myData, setMyData] = useState([]);
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const handleItemClick = (index) => {
+      setActiveIndex(index);
+    };
+
+    // Функция для обновления данных в состоянии
+    const updateData = () => {
+      const newData = [
+        {'foto': Rest_italia, title : 'Pizza Italiano', review: '4/5' },
+        {'foto': Rest_Kebab, title : 'Traditional Kebab', review: '2/5'},
+        {'foto': Rest_fish, title : 'Star Fish', review: '5/5'},
+        {'foto': Rest_Burger, title : 'Boston Burger’s', review: '3/5'}
+      ]; // Замените на свои данные
+      setMyData(newData);
+    };
+
+    useEffect(() => {
+        updateData();
+      }, []);
+
+    return (
+        <>
+        <div className='content_general'>
+            <img className='star' src={star} alt="Stars" />
+            {myData.map((item, index) => (
+                <div className='content_general_middle' key={index}>
+                     
+                     <img src={item.foto} alt={item.title} />
+                     <h3>{item.title}</h3>
+                     <p style={{ color: getRandomColor(), backgroundColor: getRandomColor(), color: getRandomColor() }}>{item.review}</p>
+                     {index !== myData.length - 1 &&  <div className='line'></div>}
+                    
+                </div>
+                
+      ))}
+                  <footer className = "footer_content">
+       <div className="fab-container">
+        <ul>
+        <li
+          className={`fab-item ${activeIndex === 0 ? 'active' : ''}`}
+          onClick={() => handleItemClick(0)}
+        >
+          <a href="#">
+          <img src={home} alt="" />
+          </a>
+         
+        </li>
+        <li
+          className={`fab-item ${activeIndex === 1 ? 'active' : ''}`}
+          onClick={() => handleItemClick(1)}
+        >
+          <a href="#">
+          <img src={like} alt="" />
+          </a>
+         
+        </li>
+        <li
+          className={`fab-item ${activeIndex === 2 ? 'active' : ''}`}
+          onClick={() => handleItemClick(2)}
+        >
+          <a href="#">
+          <img src={buscket} alt="" />
+          </a>
+         
+        </li>
+        <li
+          className={`fab-item ${activeIndex === 3 ? 'active' : ''}`}
+          onClick={() => handleItemClick(3)}
+        >
+          <a href="#">
+          <img src={notification} alt="" />
+          </a>
+         
+        </li>
+        <li
+          className={`fab-item ${activeIndex === 4 ? 'active' : ''}`}
+          onClick={() => handleItemClick(4)}
+        >
+          <a href="#">
+          <img src={profilecircle} alt="" />
+          </a>
+        </li>
+          <div className='indicator'></div>
+        </ul>
+        </div>
+      </footer>
+        </div>
+        </>
+    )
+}
+
+export default General;
